92cadd444e595946d4583d8991d7fade4e303487
Joao Paulo Araujo <paulointer077@gmail.com>
2021-09-23 21:55:58 -0400

    feat: integrating with stripe API

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
 
-export function SubcribeButton() {
+interface SubcribeButton {
+  priceId: string;
+}
+
+export function SubcribeButton({ priceId }: SubcribeButton ) {
   return (
     <button
       type="button"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,56 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { stripe } from '../services/stripe'
 import { SubcribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | Ig.news</title>
       </Head>
-      
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubcribeButton />
+          <SubcribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="ig news avatar" />
-        
+
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JcynJG4h9hjad1VKyWyvDZu');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }

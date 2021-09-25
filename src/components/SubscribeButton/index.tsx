036689a875881d37497b9380d4324ef085832207
Joao Paulo Araujo <paulointer077@gmail.com>
2021-09-24 22:30:38 -0400

    feat: creating redirect to checkout page

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stipe-js'
 import styles from './styles.module.scss'
 
 interface SubcribeButton {
@@ -5,10 +8,31 @@ interface SubcribeButton {
 }
 
 export function SubcribeButton({ priceId }: SubcribeButton ) {
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return
+    }
+    
+    try {
+      const res = await api.post('/subscribe')
+      const { sessionId } = res.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (e) {
+      alert(e.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
